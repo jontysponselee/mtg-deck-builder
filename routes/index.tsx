5dccbee9f1d30e668a7f8731ff3268a4e312e17f
Jonty Sponselee <jsponselee97@gmail.com>
2024-12-20 12:33:21 +0100

    feature: init xata

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,34 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import {getXataClient} from "../lib/xata.ts";
 
-export default function Home() {
-  const count = useSignal(3);
-  return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
-  );
+async function getPostTitles() {
+    const xata = getXataClient();
+
+    return await xata.db.posts.select(["title"]).getAll();
+}
+
+export default async function Home() {
+    const postTitles = await getPostTitles();
+
+    return (
+        <div class="px-4 py-8 mx-auto bg-[#86efac]">
+            <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+                <h1 class="text-4xl font-bold">Welcome to MTG Deck Builder</h1>
+                <p class="my-4">
+                    This is an example and will be removed later on.
+                </p>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {postTitles.map(({title}) => (
+                        <tr><td>{title}</td></tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
